61f8327bbc6cca9781be9af8806265d6896ab589
Xyhff <109801126+Xyhff@users.noreply.github.com>
2022-09-11 14:50:54 +0800

    changed_1

--- main.go
+++ main.go
@@ -239,7 +239,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:      append([]string{*adana}, "XY", "qwq", "awa", "qaq"),
+		NickName:      append([]string{*adana}, "XY", "qwq", "awa", "qAq"),
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
